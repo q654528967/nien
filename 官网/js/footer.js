c7ad485213b254e4ac189edd28ead0f3222bcd9e
leiping <654528967@qq.com>
2020-04-08 14:03:38 +0800

    1版

---
+++ 官网/js/footer.js
@@ -0,0 +1,40 @@
+{
+  let $html=`<footer class="pt-5" style="background: #222831; color:#fff;">
+  <div class="container">
+      <div class="row pb-5">
+          <div class="col-md">
+              <h5 style="white-space: nowrap;">尼恩供应链管理（上海）有限公司</h5>
+              <ul class="list-unstyled mt-4">
+                  <li><a href="./aboutMe.html">关于我们</a></li>
+                  <li><a href="./Contact.html">联系我们</a></li>
+              </ul>
+          </div>
+          <div class="col-md">
+              <h5>服务内容</h5>
+              <ul class="list-unstyled mt-4">
+                  <li><a href="javascript:;">关于我们</a></li>
+                  <li><a href="javascript:;">联系我们</a></li>
+              </ul>
+          </div>
+          <div class="col-md">
+              <h5>服务支持</h5>
+              <ul class="list-unstyled mt-4">
+                  <li><a href="javascript:;">电话1: 400-1122-385</a></li>
+                  <li><a href="javascript:;">电话2: 021-80338029</a></li>
+                  <li><a href="javascript:;">邮 箱: janson@aknien.net</a></li>
+              </ul>
+          </div>
+          <div class="col-md-4 text-center">
+              <h5>尼恩有限公司公众号</h5>
+              <div class="m-auto" style="width:9em;">
+                <img class="img-fluid" src="../img/index/erweima.jpg" />
+              </div>
+          </div>
+      </div>
+      <div class="row justify-content-center">
+          <p style="font-size:12px;">Copyright © 2002-2018 尼恩供应链管理（上海）有限公司 版权所有</p>
+      </div>
+    </div>
+  </footer>`
+$('body').append($html)
+}
--- 官网/js/header.js
+++ 官网/js/header.js
@@ -1,7 +1,7 @@
 (function(){
-let $header=`<header>
-<nav class="navbar navbar-expand-md navbar-light p-0">
-        <a class="navbar-brand p-0" style="width: 9vmax;" href="#">
+let $header=`<header class="container p-0">
+<nav class="navbar navbar-expand-lg navbar-light p-0">
+        <a class="navbar-brand p-0" style="width:4em;" href="./index.html">
             <img class="img-fluid" src="../img/index/logo.gif" alt="">
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#nav" aria-controls="nav" aria-expanded="false" aria-label="Toggle navigation">
@@ -9,35 +9,70 @@ let $header=`<header>
         </button>
         <div class="collapse navbar-collapse" id="nav">
           <ul class="navbar-nav ml-auto">
-            <li class="nav-item active">
-              <a class="nav-link" href="#">首页</a>
+            <li class="nav-item">
+              <a class="nav-link" href="./index.html" class="index">首页</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="./business.html">业务范围</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="./businessSelect.html">一键报价</a>
             </li>
             <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">业务范围</a>
-              <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                <a class="dropdown-item" href="#">Action</a>
-                <a class="dropdown-item" href="#">Another action</a>
-                <a class="dropdown-item" href="#">Something else here</a>
+              <a class="nav-link dropdown-toggle" href="javascript:;" id="yjcx" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">一键查询</a>
+              <div class="dropdown-menu" aria-labelledby="yjcx">
+                <a class="dropdown-item" href="javascript:;">业务查询</a>
+                <a class="dropdown-item" href="javascript:;">运价查询</a>
+                <a class="dropdown-item" href="javascript:;">目的港查询</a>
+                <a class="dropdown-item" href="javascript:;">hs编码查询</a>
+                <a class="dropdown-item" href="javascript:;">美国hs编码查询</a>
+                <a class="dropdown-item" href="javascript:;">机场查询</a>
+                <a class="dropdown-item" href="javascript:;">通关查询</a>
+              </div>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="javascript:;" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">我要发货</a>
+              <div class="dropdown-menu" aria-labelledby="yjcx">
+                <a class="dropdown-item" href="./deliverGoods.html">集运仓</a>
+                <a class="dropdown-item" href="javascript:;">FBA全程</a>
+                <a class="dropdown-item" href="javascript:;">美国海运拼货</a>
+                <a class="dropdown-item" href="javascript:;">慢船拼货</a>
+                <a class="dropdown-item" href="javascript:;">空运拼货</a>
+                <a class="dropdown-item" href="javascript:;">美国尾程</a>
+                <a class="dropdown-item" href="javascript:;">国际快递</a>
+              </div>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="javascript:;" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">新闻资讯</a>
+              <div class="dropdown-menu" aria-labelledby="yjcx">
+                <a class="dropdown-item" href="./news.html">国际快递新闻</a>
+                <a class="dropdown-item" href="javascript:;">跨境物流新闻</a>
+                <a class="dropdown-item" href="javascript:;">国际货运优惠</a>
+                <a class="dropdown-item" href="javascript:;">新客户体验</a>
               </div>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">我要发货</a>
+              <a class="nav-link" href="./checkPrice.html">网点查询</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">进口贸易</a>
+              <a class="nav-link" href="./logisticsTracking.html">物流跟踪</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">联系我们</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">价格查询</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">服务介绍</a>
+              <a class="nav-link" href="../html/login.html">用户中心</a>
             </li>
           </ul>
         </div>
       </nav>
 </header>`
 $('body').prepend($header);
+let url=window.location.href
+$('#nav li>a').each((index,el)=>{
+  let elHref=el.href;
+  let code=url.includes(elHref)
+  if(code==true){
+    el.style.color="#ff6600"
+  }
+
+})
+
 })() 
